a94200a806c1dbf024cc12e4992fb98c8d39492e
Salvador Diaz <163175917+slvDiaz@users.noreply.github.com>
2024-11-10 19:37:56 -0600

    Add files via upload

---
+++ options.js
@@ -0,0 +1,24 @@
+function optionsRdb() {
+	var orden = document.getElementById('orden');
+	var random = document.getElementById('random');
+	if (orden.checked) {
+		location.href = "ordenQuestion.html";
+	}
+	if (random.checked) {
+		location.href = "https://www.youtube.com/watch?v=7h2ryr_uUEs";
+	}
+	if (orden.checked == false && random.checked == false) {
+		alert("Selecciona una opcion!");
+	}
+}
+
+function changeColor(rdb) {
+	if (rdb===1) {
+		document.getElementById('lblOrden').style.backgroundColor= '#fff';
+		document.getElementById('lblRandom').style.backgroundColor= '#fbe9db';
+	}
+	else{
+		document.getElementById('lblRandom').style.backgroundColor= '#fff';
+		document.getElementById('lblOrden').style.backgroundColor= '#fbe9db';
+	}
+}
